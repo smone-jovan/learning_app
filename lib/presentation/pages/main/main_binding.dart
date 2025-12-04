a750cf1509b8c78bd7a1a2f91b98be09cc9440d4
smone-jovan <craftxbox21@gmail.com>
2025-12-04 11:59:53 +0700

    Add QuizController to MainBinding

--- lib/presentation/pages/main/main_binding.dart
+++ lib/presentation/pages/main/main_binding.dart
@@ -1,14 +1,20 @@
 import 'package:get/get.dart';
 import '../../controllers/home_controller.dart';
 import '../../controllers/main_controller.dart';
+import '../../controllers/quiz_controller.dart';
+import '../../controllers/gamification_controller.dart';
 
 class MainBinding extends Bindings {
   @override
   void dependencies() {
-    // Lazy load - jangan langsung initialize
+    // Main Controllers - lazy load
     Get.lazyPut<MainController>(() => MainController());
     Get.lazyPut<HomeController>(() => HomeController());
     
-    // AuthController hanya load saat login page
+    // ✅ Feature Controllers - needed for bottom navigation tabs
+    Get.lazyPut<QuizController>(() => QuizController());
+    Get.lazyPut<GamificationController>(() => GamificationController());
+    
+    // AuthController loaded separately at login
   }
 }
